e1a0ffb572eadcd95201f2509a8c259b29350460
LÊ HOÀNG VIỄN <15521006@ms.uit.edu.vn>
2020-06-05 11:00:36 +0700

    Updates

---
+++ precache-manifest.9eec9a71e0c87df2e7172967b8b061af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "101e475cc4daf942d34e582e579f9069",
+    "url": "/api-react/index.html"
+  },
+  {
+    "revision": "8f8a460501327343f020",
+    "url": "/api-react/static/css/main.c8cad5be.chunk.css"
+  },
+  {
+    "revision": "c2c04c90cca7ee34e57b",
+    "url": "/api-react/static/js/2.4195ceb7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/api-react/static/js/2.4195ceb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f8a460501327343f020",
+    "url": "/api-react/static/js/main.ba9024ac.chunk.js"
+  },
+  {
+    "revision": "89302caeee1d6ba83990",
+    "url": "/api-react/static/js/runtime-main.f10a773c.js"
+  }
+]);
